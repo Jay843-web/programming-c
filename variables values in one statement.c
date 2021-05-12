1d1652fd96d3dfdd3470b1df16d7b09b6729822c
Jay843-web <83710014+Jay843-web@users.noreply.github.com>
2021-05-12 23:29:43 +0530

    Create variables values in one statement.c

---
+++ variables values in one statement.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+   int var2;
+   int var3;
+   var2=60;
+   var3=40;
+   printf("Value of var2 is %d and var3 is %d", var2, var3);
+   return 0;
+
+}
+© 2021 GitHu
